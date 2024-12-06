8dcf7180a4e94192a0d9168fcb5f5e15848f1f07
xiaofan <xiaofan.ma@ezynet.sg>
2024-12-06 17:30:46 +0800

    updated

--- blog/js/custom.js
+++ blog/js/custom.js
@@ -1,15 +1,8 @@
-// import "./mayang.js";
+
+// ****************** 樱花特效 ******************
+import "./mayang.js";
+// ****************** 樱花特效 ******************
 
 // ****************** 星空背景和流星特效 ******************
-function _inituniverse() {
-  var canvas = document.createElement("canvas");
-  canvas.id = "universe";
-  document.body.appendChild(canvas);
-  
-}
-
-_inituniverse();
-
 import "./universe.js"; 
-
 // ****************** 星空背景和流星特效 ******************
