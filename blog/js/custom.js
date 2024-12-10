413902705ab3c9a229ba762f8ddb8c30d6bfd11a
xiaofan <xiaofan.ma@ezynet.sg>
2024-12-10 17:21:24 +0800

    更改为本地存储

--- blog/js/custom.js
+++ blog/js/custom.js
@@ -8,12 +8,29 @@ import "./universe.js";
 
 // ****************** 背景更新 ******************
 (() => {
-  fetch('http://101.200.151.47:8848/api/get_image_path', {
-    method: 'Get',
-  }).then(response => {
-    return response.json();
-  }).then(data => {
-    document.getElementById("#web_bg").style.background = data.data.image_path;
-  })
+  // fetch('http://101.200.151.47:8848/api/get_image_path', {
+  //   method: 'Get',
+  // }).then(response => {
+  //   return response.json();
+  // }).then(data => {
+  //   document.getElementById("#web_bg").style.background = data.data.image_path;
+  // })
+  const itemStr = localStorage.getItem(key);
+
+  // 如果没有找到数据，则返回 null
+  if (!itemStr) {
+    return;
+  }
+
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+
+  // 如果数据过期了，清除并返回 null
+  if (now.getTime() > item.expiry) {
+    localStorage.removeItem(key);
+    return;
+  }
+
+  document.getElementById("#web_bg").style.background = item.value;
 })();
 // ****************** 背景更新 ******************
