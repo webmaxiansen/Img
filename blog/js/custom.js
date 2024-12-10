93b4797045b16c474c50e4335c1c98123444d2cf
xiaofan <xiaofan.ma@ezynet.sg>
2024-12-10 17:26:43 +0800

    更新key

--- blog/js/custom.js
+++ blog/js/custom.js
@@ -15,7 +15,7 @@ import "./universe.js";
   // }).then(data => {
   //   document.getElementById("#web_bg").style.background = data.data.image_path;
   // })
-  const itemStr = localStorage.getItem(key);
+  const itemStr = localStorage.getItem("image_path");
 
   // 如果没有找到数据，则返回 null
   if (!itemStr) {
