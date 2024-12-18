c53dd1cc8cdcf514787a9c357fee71653f83e4f3
xiaofan <xiaofan.ma@ezynet.sg>
2024-12-18 10:59:31 +0800

    恢复top_img

--- blog/js/custom.js
+++ blog/js/custom.js
@@ -20,40 +20,40 @@ import "./title.js";
   //   console.log("data", data.data.image_path);
   // })
 
-  let LOCAL_BG_URL = "https://images.maxiansen.top/blog/public/img/dm7.webp";
+  // let LOCAL_BG_URL = "https://images.maxiansen.top/blog/public/img/dm7.webp";
 
-  let IS_OPEN_ONLINE_BG_CONFIG = true;
+  // let IS_OPEN_ONLINE_BG_CONFIG = true;
 
-  const itemStr = localStorage.getItem("image_path");
+  // const itemStr = localStorage.getItem("image_path");
 
-  // 如果没有找到数据，则返回 null
-  if (!itemStr) {
-    return;
-  }
+  // // 如果没有找到数据，则返回 null
+  // if (!itemStr) {
+  //   return;
+  // }
 
-  const item = JSON.parse(itemStr);
-  const now = new Date();
+  // const item = JSON.parse(itemStr);
+  // const now = new Date();
 
-  // 如果数据过期了，清除并返回 null
-  if (now.getTime() > item.expiry) {
-    localStorage.removeItem(key);
-    return;
-  }
+  // // 如果数据过期了，清除并返回 null
+  // if (now.getTime() > item.expiry) {
+  //   localStorage.removeItem(key);
+  //   return;
+  // }
 
 
 
-  if (IS_OPEN_ONLINE_BG_CONFIG) {
-    // fetch('http://101.200.151.47/update_bg/data.json')
-    //   .then((response) => response.json())
-    //   .then((data) => LOCAL_BG_URL = data.image_path);
+  // if (IS_OPEN_ONLINE_BG_CONFIG) {
+  //   // fetch('http://101.200.151.47/update_bg/data.json')
+  //   //   .then((response) => response.json())
+  //   //   .then((data) => LOCAL_BG_URL = data.image_path);
 
-  } else {
-    LOCAL_BG_URL = item.value;
-  }
+  // } else {
+  //   LOCAL_BG_URL = item.value;
+  // }
 
-  document.getElementById("web_bg").style.background = `url(${LOCAL_BG_URL})`;
-  document.getElementById("web_bg").style.backgroundSize = 'cover';
-  document.getElementById("web_bg").style.backgroundPosition = 'center';
+  // document.getElementById("web_bg").style.background = `url(${LOCAL_BG_URL})`;
+  // document.getElementById("web_bg").style.backgroundSize = 'cover';
+  // document.getElementById("web_bg").style.backgroundPosition = 'center';
 
 })();
 // ****************** 背景更新 ******************
